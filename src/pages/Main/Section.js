107b66bdd6fa9a8a1046c05b1c724143cf6c2ce4
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-06 16:17:57 +0900

    Add : 타이틀 완성

--- src/pages/Main/Section.js
+++ src/pages/Main/Section.js
@@ -6,7 +6,19 @@ class Section extends Component {
     return (
       <section>
         <div className="titleBanner">
-          <img src="./images/main/title/title01.png" />
+          <div class="titleText01">
+            <h2>딸기의 변신은 무죄!</h2>
+            <p className="smallText">
+              딸기 향을 만끽하며
+              <br />
+              씻어 봐요
+            </p>
+          </div>
+          <img
+            className="titleBannerImg"
+            src="./images/main/title/title001.png"
+            alt="strawberry title"
+          />
         </div>
         <article className="contentWrap">
           <div className="productWrap">
