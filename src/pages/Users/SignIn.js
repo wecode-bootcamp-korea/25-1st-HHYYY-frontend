5dc98cdd127782e7470031e79d7808085660abc0
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-07 18:30:13 +0900

    add: sign in 레이아웃 작업 시작

--- src/pages/Users/SignIn.js
+++ src/pages/Users/SignIn.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
+import './signin.scss';
 
 class SignIn extends Component {
   render() {
-    return <div>null</div>;
+    return (
+      <>
+        <Header />
+        <article className="signIn">
+          <div className="subject">
+            <h1>로그인</h1>
+          </div>
+          <div className="signInBox">
+            <form>
+              <div className="InputSignIn">
+                <input className="idInput" type="text" placeholder="아이디" />
+                <input
+                  className="pwInput"
+                  type="password"
+                  placeholder="비밀번호"
+                />
+              </div>
+              <div>
+                <button>로그인</button>
+              </div>
+              <div className="signup">
+                <button className="/">회원가입</button>
+              </div>
+            </form>
+          </div>
+        </article>
+      </>
+    );
   }
 }
 
