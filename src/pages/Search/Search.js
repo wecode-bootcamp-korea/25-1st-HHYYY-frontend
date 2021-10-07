769c01723f01a14689d124cbd5a9c2a7405dec72
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-07 18:35:32 +0900

    add: feature/header에서 search 관련 파일 이관

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
+import './search.scss';
+
 class Search extends Component {
   render() {
-    return <div>null</div>;
+    return (
+      <div className="search">
+        <input
+          className="searchInput"
+          type="search"
+          placeholder="더 풍성하게 돌아온 NEW 기프트"
+        />
+        <button className="searchBtn">
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
+    );
   }
 }
 
