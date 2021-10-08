b6c0616ee0bcc2ca108ab03772ddfd57bd1a875c
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-08 10:38:13 +0900

    Add: 아이콘 사이즈 수정 중

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -12,7 +12,9 @@ class Search extends Component {
           placeholder="더 풍성하게 돌아온 NEW 기프트"
         />
         <button className="searchBtn">
-          <i className="fas fa-search"></i>
+          <i className="fas fa-search">
+            <div className="empty"></div>
+          </i>
         </button>
       </div>
     );
