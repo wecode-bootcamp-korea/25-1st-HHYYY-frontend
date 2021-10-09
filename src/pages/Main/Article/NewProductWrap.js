0eeed3be2d8c313952731c042fc39990f050e490
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-09 15:05:44 +0900

    Fix : 코드리뷰 피드백 article -> productList에서 일부 반영

--- src/pages/Main/Article/NewProductWrap.js
+++ src/pages/Main/Article/NewProductWrap.js
@@ -4,27 +4,26 @@ import './NewProductWrap.scss';
 class newProductWrap extends Component {
   render() {
     return (
-      <>
-        <article className="newProductWrap">
-          <div className="productText">
-            <h2>
-              NEW 탱글드 헤어
-              <br />
-              트리트먼트
-            </h2>
-            <p>
-              전통 악기에서 영감을 받은 두피 트리트먼트로, 기분 좋은 낮잠에
-              빠져들듯 부드럽고 편안한 시간을 선사합니다.
-            </p>
-          </div>
-          <div className="productImg">
-            <img
-              src="./images/main/banner/middle/longhair.jpg"
-              alt="long hair"
-            />
-          </div>
-        </article>
-      </>
+      <article className="newProductWrap">
+        <div className="productText">
+          <h2 className="productText_big_text">
+            NEW 탱글드 헤어
+            <br />
+            트리트먼트
+          </h2>
+          <p className="productText_small_text">
+            전통 악기에서 영감을 받은 두피 트리트먼트로, 기분 좋은 낮잠에
+            빠져들듯 부드럽고 편안한 시간을 선사합니다.
+          </p>
+        </div>
+        <div className="productImg_longHair">
+          <img
+            className="productImg_longHair_img"
+            src="./images/main/banner/middle/longhair.jpg"
+            alt="long hair"
+          />
+        </div>
+      </article>
     );
   }
 }
