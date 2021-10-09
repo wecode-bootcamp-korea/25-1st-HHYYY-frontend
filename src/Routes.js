d3af6b0d3a868b977989ce602de8b5fddf046ccc
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-09 18:27:44 +0900

    Add: Routes에 경로수정

--- src/Routes.js
+++ src/Routes.js
@@ -5,8 +5,8 @@ import Main from './pages/Main/Main';
 import ProductCategory from './pages/Product/ProductCategory';
 import ProductDetail from './pages/Product/ProductDetail';
 import Search from './pages/Search/Search';
-import Signup from './pages/Users/SignUp';
-import Signin from './pages/Users/SignIn';
+import SignUp from './pages/Users/SignUp';
+import SignIn from './pages/Users/SignIn';
 import Cart from './pages/Cart/Cart';
 
 class Routes extends Component {
@@ -18,8 +18,8 @@ class Routes extends Component {
           <Route exact path="/product-category" component={ProductCategory} />
           <Route exact path="/product-detail" component={ProductDetail} />
           <Route exact path="/search" component={Search} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signin" component={SignIn} />
           <Route exact path="/cart" component={Cart} />
         </Switch>
       </Router>
