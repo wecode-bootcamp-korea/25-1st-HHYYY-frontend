af5753dd1a7962860cc499e80e20f107efc946af
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-09 16:50:59 +0900

    Add: 인풋창 아이콘추가완료

--- src/pages/Users/SignIn/SignInBox.js
+++ src/pages/Users/SignIn/SignInBox.js
@@ -1,29 +1,40 @@
 import React, { Component } from 'react';
+import './signinBox.scss';
 
 class SignInBox extends Component {
   render() {
     const { onChange, inputIdValue, inputPwValue, handleLoginBtn } = this.props;
     return (
-      <div className="InputSignIn">
-        <input
-          name="inputIdValue"
-          className="idInput"
-          type="text"
-          placeholder="아이디"
-          onChange={onChange}
-          onKeyUp={handleLoginBtn}
-          value={inputIdValue}
-        />
-        <input
-          name="inputPwValue"
-          className="pwInput"
-          type="password"
-          placeholder="비밀번호"
-          onChange={onChange}
-          onKeyUp={handleLoginBtn}
-          value={inputPwValue}
-        />
-      </div>
+      <>
+        <div className="InputSignIn">
+          <span>
+            <i className="fas fa-user-circle fa-lg" />
+          </span>
+          <input
+            name="inputIdValue"
+            className="idInput"
+            type="text"
+            placeholder="아이디"
+            onChange={onChange}
+            onKeyUp={handleLoginBtn}
+            value={inputIdValue}
+          />
+        </div>
+        <div className="InputSignIn">
+          <span>
+            <i className="fas fa-lock fa-lg" />
+          </span>
+          <input
+            name="inputPwValue"
+            className="pwInput"
+            type="password"
+            placeholder="비밀번호"
+            onChange={onChange}
+            onKeyUp={handleLoginBtn}
+            value={inputPwValue}
+          />
+        </div>
+      </>
     );
   }
 }
