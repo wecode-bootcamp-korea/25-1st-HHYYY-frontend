ee35325112a38532430b0cae121ba46754168fe9
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-09 14:18:58 +0900

    Fix : 클래스명 변경 리팩토링1

--- src/pages/Main/Article/BannerWrap.js
+++ src/pages/Main/Article/BannerWrap.js
@@ -5,37 +5,51 @@ class bannerWrap extends Component {
   render() {
     return (
       <article className="bannerWrap">
-        <div className="banner1">
-          <h2>
-            <span class="abocado">아보카도</span>는{' '}
+        <section className="leftBanner">
+          <h2 className="leftBanner_text">
+            <span class="abocado">아보카도</span>는&nbsp;
             <span class="piboo">피부</span>에 양보하세요
           </h2>
-          <img src="./images/main/banner/middle/banner01.jpg" alt="banner1" />
-        </div>
-        <div className="banner2">
-          <h2>제주로부터 녹차팩이 도착했어요!</h2>
-          <img src="./images/main/banner/middle/banner02.jpg" alt="banner2" />
-        </div>
-        <div className="banner3">
-          <h2>
+          <img
+            className="leftBanner_img"
+            src="./images/main/banner/middle/banner01.jpg"
+            alt="leftBanner_img"
+          />
+        </section>
+        <section className="rightBannerTop">
+          <h2 className="rightBannerTop_text">
+            제주로부터 녹차팩이 도착했어요!
+          </h2>
+          <img
+            className="rightBannerTop_img"
+            src="./images/main/banner/middle/banner02.jpg"
+            alt="rightBannerTop_img"
+          />
+        </section>
+        <section className="rightBannerBottom_left">
+          <h2 className="rightBannerBottom_left_text">
             딸기 왕자를
             <br />
             아시나요?
           </h2>
           <img
             src="./images/main/banner/middle/banner05.jpg"
-            alt="banner3_img"
-            className="banner3_img"
+            alt="rightBannerBottom_left_img"
+            className="rightBannerBottom_left_img"
           />
-        </div>
-        <div className="banner4">
-          <h2>
+        </section>
+        <section className="rightBannerBottom_right">
+          <h2 className="rightBannerBottom_right_text">
             가입시 포인트
             <br />
             5000원 증정
           </h2>
-          <img src="./images/main/banner/middle/banner03.jpg" alt="banner3" />
-        </div>
+          <img
+            className="rightBannerBottom_right_img"
+            src="./images/main/banner/middle/banner03.jpg"
+            alt="rightBannerBottom_right_img"
+          />
+        </section>
       </article>
     );
   }
