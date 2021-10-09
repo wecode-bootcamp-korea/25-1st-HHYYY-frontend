8794960374a39a1f37468cb8fd5f1f4bb031bb4b
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-09 20:35:01 +0900

    Add: 장바구니 레이아웃 구현 중

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
+import { Link } from 'react-router-dom';
+
+import './cart.scss';
 
 class Cart extends Component {
   render() {
-    return <div>null</div>;
+    return (
+      <>
+        <Header />
+        <article className="Cart">
+          <header>
+            <h1>SHOPPING CART</h1>
+            <p>Cart</p>
+          </header>
+          <section>
+            <form>
+              <h3>제품</h3>
+              <table>
+                <thead>
+                  <tr>
+                    <td>장바구니에 담겨있는 상품이 없습니다.</td>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </form>
+          </section>
+          <section>
+            <div className="priceBox">
+              <p>총</p>
+            </div>
+          </section>
+        </article>
+      </>
+    );
   }
 }
 
