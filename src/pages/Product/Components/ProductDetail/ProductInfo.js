cf934dc8cdeee6bf016db728eb631d0c8f0db81b
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-11 03:04:12 +0900

    Add : 상수데이터로 Good To Know 항목 맵핑

--- src/pages/Product/Components/ProductDetail/ProductInfo.js
+++ src/pages/Product/Components/ProductDetail/ProductInfo.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import GoodToKnow from './GoodToKnow';
 import './ProductInfo.scss';
 
 class ProductInfo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      classON: false,
+    };
+  }
+
+  GoodToKnowBtnToggle = () => {
+    const { classON } = this.state;
+    this.setState({
+      classON: !classON,
+    });
+  };
+
   render() {
+    const { classON } = this.state;
     return (
       <>
         <article className="productInfo">
@@ -19,12 +35,18 @@ class ProductInfo extends Component {
               <p className="itemTag">#배쓰밤 #환상적인우주느낌</p>
             </header>
             <div className="itemInfo">
+              <ul className={classON ? 'markON' : 'markOFF'}>
+                {VEGAN_LIST.map(data => (
+                  <GoodToKnow key={data.id} data={data} />
+                ))}
+              </ul>
               <p className="goodToKnow">
                 Good to Know
                 <img
                   src="/images/product/question.png"
                   className="questionMark"
                   alt="questionMark"
+                  onClick={this.GoodToKnowBtnToggle}
                 />
               </p>
               <ul className="itemInfoList">
@@ -102,5 +124,35 @@ class ProductInfo extends Component {
     );
   }
 }
+
+// 상품정보가 아닌, 고정된 상수 데이터는 여기에다 호출하는게 맞는지? 아니면 Public/data에 json 파일로 따로 분리해주는게 맞는지?
+const VEGAN_LIST = [
+  {
+    id: 1,
+    veganHeader: '비건(Vegan)',
+    content:
+      '동물성 원료를 전혀 포함하지 않은 영국 비건협회 인증된 제품입니다.',
+    imgSrc: '/images/product/vegan1.png',
+  },
+  {
+    id: 2,
+    veganHeader: '베지테리언(Vegetarian)',
+    content:
+      '우유, 달걀, 꿀을 제외한 동물성 원료를 포함하지 않은 영국 베지테리언 협회 인증 제품입니다.',
+    imgSrc: '/images/product/vegan2.png',
+  },
+  {
+    id: 3,
+    veganHeader: '베지테리언에게 적합한 제품(Suitable for Vegetarians)',
+    content: '동물성 원료를 전혀 포함하지 않은 제품입니다.',
+    imgSrc: '/images/product/vegan3.png',
+  },
+  {
+    id: 4,
+    veganHeader: '셀프-프리저빙(Self-Preserving)',
+    content: '자연에서 얻은 원재료로 스스로 보존 가능한 제품입니다.',
+    imgSrc: '/images/product/vegan4.png',
+  },
+];
 
 export default ProductInfo;
