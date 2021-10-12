0ea7f2068fc2e0b2b180f58754fb9576646f6ed0
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-12 19:34:37 +0900

    Add: search페이지내용삭제

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -4,18 +4,7 @@ import './search.scss';
 
 class Search extends Component {
   render() {
-    return (
-      <div className="search">
-        <input
-          className="searchInput"
-          type="search"
-          placeholder="더 풍성하게 돌아온 NEW 기프트"
-        />
-        <button className="searchBtn">
-          <i className="fas fa-search"></i>
-        </button>
-      </div>
-    );
+    return null;
   }
 }
 
