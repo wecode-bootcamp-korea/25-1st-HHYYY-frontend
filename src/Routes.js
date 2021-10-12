8e5c2615012c1cc1dabd45cf2fdb14d0e2302f95
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-12 19:59:13 +0900

    Add: 공용폴더내용

--- src/Routes.js
+++ src/Routes.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
 import ProductCategory from './pages/Product/ProductCategory';
 import ProductDetail from './pages/Product/ProductDetail';
 import Search from './pages/Search/Search';
-import SignUp from './pages/Users/SignUp';
-import SignIn from './pages/Users/SignIn';
+import Signup from './pages/Users/Signup';
+import Signin from './pages/Users/Signin';
 import Cart from './pages/Cart/Cart';
+import Footer from './components/Footer/Footer';
 
 class Routes extends Component {
   render() {
     return (
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/main" component={Main} />
           <Route exact path="/product-category" component={ProductCategory} />
           <Route exact path="/product-detail" component={ProductDetail} />
           <Route exact path="/search" component={Search} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/signin" component={Signin} />
           <Route exact path="/cart" component={Cart} />
         </Switch>
+        <Footer />
       </Router>
     );
   }
