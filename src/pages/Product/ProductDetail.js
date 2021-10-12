2fa6cdc0930451973637470dc055adec2723c384
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-12 21:12:48 +0900

    Fix : sider (switch-case) 에러 수정

--- src/pages/Product/ProductDetail.js
+++ src/pages/Product/ProductDetail.js
@@ -29,22 +29,16 @@ class ProductDetail extends Component {
       // average.parseInt();
       case 1:
         return '★';
-        break;
       case 2:
         return '★★';
-        break;
       case 3:
         return '★★★';
-        break;
       case 4:
         return '★★★★';
-        break;
       case 5:
         return '★★★★★';
-        break;
       default:
         return '★★★★★';
-        break;
     }
   };
 
