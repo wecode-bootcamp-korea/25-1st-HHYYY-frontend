696fc90dbc4127f8b11bcb2c943a41f7f4c00ddd
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-13 08:14:47 +0900

    Fix : 파일명 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
 import ProductCategory from './pages/Product/ProductCategory';
