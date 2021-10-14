e272fd0038bd526362c319edaa88b32f67bdfe0c
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-14 10:09:30 +0900

    Fix : 카테고리 nav 경로 수정

--- src/pages/Product/Components/ProductNav.js
+++ src/pages/Product/Components/ProductNav.js
@@ -12,7 +12,7 @@ class ProductNav extends Component {
           {navMenuInfo.sub_categories?.map(subInfo => {
             return (
               <li className="list">
-                <Link to="/product-category/${subInfo.sub_category_id}">
+                <Link to={`/product-category/${subInfo.sub_category_id}`}>
                   {subInfo.sub_category_name} &#40;
                   {subInfo.sub_category_products_count}&#41;
                 </Link>
