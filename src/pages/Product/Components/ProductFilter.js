33e870995a781274a17b56e1b06eae5f926b1eca
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-14 18:38:25 +0900

    fix: 수정

--- src/pages/Product/Components/ProductFilter.js
+++ src/pages/Product/Components/ProductFilter.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+// import { API } from '../../../config.js';
 
 class ProductFilter extends Component {
-  // sortingSoon = () => {
+  constructor() {
+    super();
+    this.state = {
+      sorting: [],
+    };
+  }
 
-  // }
+  // sortingSoon = () => {
+  //   fetch(`${API.PRODUCT_DETAIL}?${this.props.match.params.id}`)
+  //     .then(res => res.json())
+  //     .then(sortingInfo =>
+  //       this.setState({
+  //         sorting: sortingInfo.product_info,
+  //       })
+  //     );
+  // };
 
   render() {
     const { navMenuInfo } = this.props;
@@ -13,9 +27,7 @@ class ProductFilter extends Component {
           총 {navMenuInfo.category_products_count}개
         </p>
         <ul className="productFilter">
-          <li className="filterList" onClick="this.sortingSoon">
-            최신순
-          </li>
+          <li className="filterList">최신순</li>
           <li className="filterList">리뷰 많은 순</li>
           <li className="filterList">낮은 가격순</li>
           <li className="filterList">높은 가격순</li>
