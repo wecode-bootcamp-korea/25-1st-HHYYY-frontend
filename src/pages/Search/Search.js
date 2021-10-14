e16bebe303fe145fc77a273b76058e112dd76fd5
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-14 10:00:45 +0900

    Add: 검색창 결과페이지와 연결

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,16 +1,64 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import './search.scss';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputSearch: [],
+      searchWord: '',
+      isSearch: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('#')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ inputSearch: data });
+      });
+  }
+
+  handleSearchWord = e => {
+    this.setState({
+      searchWord: e.target.value,
+    });
+  };
+
+  handelerSearch = () => {
+    const { isSearch } = this.state;
+    this.setState({ isSearch: !isSearch });
+  };
+
+  handleSearchResult = e => {
+    const { searchWord } = this.state;
+    e.preventDefault();
+    if (searchWord.length > 0) {
+      this.props.history.push('/search-result');
+    }
+  };
+
   render() {
+    // console.log(this.state.searchWord);
+    // const { inputSearch, searchWord } = this.state;
+    // const filtered = inputSearch.filter(searchdata =>
+    //   searchdata.name.includes(searchWord)
+    // );
     return (
       <div className="search">
         <input
+          name="searched"
           className="searchInput"
           type="search"
           placeholder="더 풍성하게 돌아온 NEW 기프트"
+          onChange={this.handleSearchWord}
         />
-        <button className="searchBtn">
+        <button
+          className="searchBtn"
+          onClick={this.handleSearchResult}
+          searchWord={this.state.searchWord}
+        >
           <i className="fas fa-search fa-2x" />
         </button>
       </div>
@@ -18,4 +66,4 @@ class Search extends Component {
   }
 }
 
-export default Search;
+export default withRouter(Search);
